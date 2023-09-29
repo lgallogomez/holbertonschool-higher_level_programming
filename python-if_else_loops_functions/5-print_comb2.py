6c31e18941dcafa7371f786a2d8b3482dc46ff3f
lgallogomez <l.gallogomez@gmail.com>
2023-09-29 14:14:51 -0500

    update 5- file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(00, 100):
+    form_n = "{:02d}".format(i)
     if i != 99:
-        print("{}, ".format(i), end="")
+        print("{}, ".format(form_n), end='')
     else:
-        print("{}".format(i))
+        print("{}".format(form_n))
