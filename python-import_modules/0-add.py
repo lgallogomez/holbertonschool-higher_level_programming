621652108fa47d6267d8772a4e69ef38aa13c804
lgallogomez <l.gallogomez@gmail.com>
2023-09-29 21:33:17 -0500

    created new dir and 1st file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
+
+
 def add_f():
     a = 1
     b = 2
     add(a, b)
     print("{} + {} = {}".format(a, b, add(a, b)))
+
 
 if __name__ == "__main__":
     add_f()
