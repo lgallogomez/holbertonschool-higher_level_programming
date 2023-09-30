4b570b52987ddde23db274476c3ddc909a2ad805
lgallogomez <l.gallogomez@gmail.com>
2023-09-29 23:50:47 -0500

    created new dir and 3th file

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+from sys import argv
+
+def func():
+    i = 0
+    summ = 0
+    args = sys.argv
+
+    if len(sys.argv) == 1:
+        print("{}".format(0))
+    elif len(sys.argv) == 2:
+        print("{}".format(args[1]))
+    elif len(sys.argv) > 2:
+        for i in range(1, len(args)):
+            summ = summ + int(args[i])
+        print(summ)
+
+
+if (__name__ == "__main__"):
+    func()
