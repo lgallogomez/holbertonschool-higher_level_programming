7197b92cd095297ffc9b5a3ad442e6c73593fd0b
lgallogomez <l.gallogomez@gmail.com>
2023-09-29 23:20:20 -0500

    created new dir and 3th file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,10 +8,10 @@ def func():
     if len(sys.argv) == 1:
             print("{} arguments.".format(i))
     elif len(sys.argv) == 2:
-            print("{} argument.".format(i + 1))
+            print("{} argument:".format(i + 1))
             print("{}: {}".format((i + 1), args[i + 1]))
     elif len(sys.argv) > 2:
-        print("{} arguments.".format(len(args) - 1))
+        print("{} arguments:".format(len(args) - 1))
         for i in range(1, len(args)):
             print("{}: {}".format(i, args[i]))
 
