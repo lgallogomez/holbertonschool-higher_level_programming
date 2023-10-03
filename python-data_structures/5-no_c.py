1ccc1316f78279b7531afc726fe8ba729d60d571
lgallogomez <l.gallogomez@gmail.com>
2023-10-03 16:54:44 -0500

    tested 5th file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    targ_1 = 'c'
-    targ_2 = 'C'
-    new_str = ""
-    for char in my_string:
-        if char != targ_1 and char != targ_2:
-            new_str += char
-    return(new_str)
+    if my_string:
+        targ_1 = 'c'
+        targ_2 = 'C'
+        new_str = ""
+        for char in my_string:
+            if char != targ_1 and char != targ_2:
+                new_str += char
+        return(new_str)
