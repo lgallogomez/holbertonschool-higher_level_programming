0e764c94e3f2662f0248122171c746de9e1c58ad
lgallogomez <l.gallogomez@gmail.com>
2023-10-05 15:51:08 -0500

    updated 11 with del plus not creating a new list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_l = list()
     i = 0
     for i in range(len(my_list)):
-        if i != idx:
-            new_l.append(my_list[i])
+        if i == idx:
+            del my_list[idx]
             i += 1
-        else:
-            i += 1
-    return (new_l)
+    return (my_list)
