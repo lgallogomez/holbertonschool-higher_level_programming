50f8dfa7be334cc02f6f09111cc8840c87905b9a
lgallogomez <l.gallogomez@gmail.com>
2023-10-05 12:52:08 -0500

    updated 10 file. for starts [i] at one

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
-    for i in new_list:
+    i = 0
+    while i < len(new_list):
+        print("{}".format(new_list[i]))
         if new_list[i] % 2 == 0:
             new_list[i] = True
-            i += 1
-        elif new_list[i] % 2 != 0:
+        else: 
             new_list[i] = False
-            i += 1
+        i += 1
     return (new_list)
