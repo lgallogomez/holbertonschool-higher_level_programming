4c9521ce894a40c4fee9b7a7579037566003720f
lgallogomez <l.gallogomez@gmail.com>
2023-10-06 12:46:24 -0500

    updated 6th file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     n_columns = len(matrix[0])
 
     for j in range(n_columns):
-        print("{}".format(matrix[j]))
+        print("{:d}".format(matrix[j]))
