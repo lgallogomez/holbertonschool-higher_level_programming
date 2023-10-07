9405e067dffc2d4a584f57cc3817e7441b712dba
lgallogomez <l.gallogomez@gmail.com>
2023-10-07 10:49:32 -0500

    created project directory and 1- file

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if new_list[i] == search:    
+            my_list[i] = replace
+    return (new_list)
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
