89cd06d8b6bd1e26ce652eb18d282648e1821627
lgallogomez <l.gallogomez@gmail.com>
2023-10-10 12:52:01 -0500

    created 10th file

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
     max_key = max(a_dictionary, key=a_dictionary.get)
     return (max_key)
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
-
