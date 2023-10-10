012b766ee4a653cd5d716406b0b610aba5589a35
lgallogomez <l.gallogomez@gmail.com>
2023-10-10 17:26:08 -0500

    updated -0 file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,8 +7,3 @@ def safe_print_list(my_list=[], x=0):
         return (x)
     except:
         pass
-    
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
