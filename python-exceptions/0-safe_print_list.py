e65b2e2b60b332a3addc6be12909b1385a8c4c6e
lgallogomez <l.gallogomez@gmail.com>
2023-10-10 17:41:02 -0500

    updated 0- file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    listLen = 0
+    listl = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end='')
-            listLen += 1
+            listl += 1
         print("")
     except:
         pass
     print("")
     return (listLen)
-
-my_list = [1, 2, 3, 4]
-x = len(my_list) + 1
-
-nb_print = safe_print_list(my_list, x)
-print("{:d}".format(nb_print))
