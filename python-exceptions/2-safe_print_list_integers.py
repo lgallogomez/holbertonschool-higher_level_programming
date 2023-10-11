82a09f89ff61c157d9071d010fbc0db7d4bc5e05
lgallogomez <l.gallogomez@gmail.com>
2023-10-11 16:33:53 -0500

    updated 2- file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[idx]), end='')
             ret += 1
-        except Exception:
+        except ValueError:
             pass
     print("")
     return (ret)
