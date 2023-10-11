2ec521d4f333993e69154c08205241c50416111d
lgallogomez <l.gallogomez@gmail.com>
2023-10-11 16:39:09 -0500

    updated 2- file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[idx]), end='')
             ret += 1
-        except ValueError or TypeError:
+        except (ValueError, TypeError):
             pass
     print("")
     return (ret)
+
+my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3,4]]
+x = len(my_list)
+nb_print = safe_print_list_integers(my_list, x)
+print("{:d}".format(nb_print))
