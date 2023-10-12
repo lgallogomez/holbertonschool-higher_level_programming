6f0c556ed5a698f5f0b4bd1af05372ad07c53061
lgallogomez <l.gallogomez@gmail.com>
2023-10-11 21:19:17 -0500

    updated 3th file

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -10,9 +10,3 @@ def safe_print_division(a, b):
         if c is not None:
             print("Inside result: {:.1f}".format(c))
             return (c)
-
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
