7ef951a85af71ee4b5230e3b55b34682413e520d
lgallogomez <l.gallogomez@gmail.com>
2023-10-12 11:47:42 -0500

    created 4th file

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    j = 0
+    new_list = []
+    for i in my_l_1:
+        try:
+            result = i / my_l_2[j]
+            new_list.append(result)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        finally:
+            j += 1
+    return (new_list)
