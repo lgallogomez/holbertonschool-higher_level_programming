7378a55dae1891fce099d18db7155b1742c63925
lgallogomez <l.gallogomez@gmail.com>
2023-10-13 14:55:50 -0500

    created 2nd file

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" File where I'm creating a class """
+
+
+class Square:
+    """ class square creates a private attribute """
+    def __init__(self, __size=0):
+        self.__size = __size
