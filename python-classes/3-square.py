620ca25910b3e7564a8d339999f1912f1816562b
lgallogomez <l.gallogomez@gmail.com>
2023-10-13 16:57:54 -0500

    created 4th file

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,3 +11,7 @@ class Square:
             raise TypeError("size must be an integer")
         if __size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        a = self.__size * self.__size
+        return (a)
