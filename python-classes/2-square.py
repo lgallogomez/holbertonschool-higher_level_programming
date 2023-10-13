e2d3b6d37178bbb6e476b178b8514dabfa9afc09
lgallogomez <l.gallogomez@gmail.com>
2023-10-13 16:47:50 -0500

    created 3th file

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,3 +11,10 @@ class Square:
             raise TypeError("size must be an integer")
         if __size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        a = Square.__size ** Square.__size
+        return (a)
+
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
