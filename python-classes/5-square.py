b716bb2a70cf174aa9662ac91c7393b5a272393c
lgallogomez <l.gallogomez@gmail.com>
2023-10-17 17:12:57 -0500

    created  5th file and main

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" File where I'm creating a class """
+
+
+class Square:
+    """ class square creates a private attribute """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        a = self.__size ** 2
+        return (a)
+    
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                for i in range(0, self.__size):
+                    print("#", end='')
+                print("")
