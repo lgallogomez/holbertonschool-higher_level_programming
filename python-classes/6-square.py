1e1ba90a19aa2a3496dd7193758c49da37142b88
lgallogomez <l.gallogomez@gmail.com>
2023-10-18 14:13:12 -0500

    updated last file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,7 +46,8 @@ class Square:
             for i in range(0, self.__position[1]):
                 print("\n", end='')
             for i in range(0, self.__size):
-                print(" ", end='')
+                for i in range(0, self.__position[0]):
+                    print(" ", end='')
                 for i in range(0, self.__size):
                     print("#", end='')
                 print("")
