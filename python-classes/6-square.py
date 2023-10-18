878f9eae07575e4434b1728f8927bfda8d07e9eb
lgallogomez <l.gallogomez@gmail.com>
2023-10-18 11:44:27 -0500

    created and tested last file

---
+++ python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+""" File where I'm creating a class """
+
+
+class Square:
+    """ class square creates a private attribute """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        a = self.__size ** 2
+        return (a)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__position[1]):
+                print("\n", end='')
+            for i in range(0, self.__size):
+                print(" ", end='')
+                for i in range(0, self.__size):
+                    print("#", end='')
+                print("")
