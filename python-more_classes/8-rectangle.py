d56bbda915bfe6f9824cec357d60eaf28d9bba6e
lgallogomez <l.gallogomez@gmail.com>
2023-10-19 18:03:19 -0500

    created 8th file

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -68,9 +68,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance")
-        if area(rect_1) == area(rect_2):
+        if rect_1.area() == rect_2.area():
             return (rect_1)
-        if area(rect_1) > area(rect_2):
+        if rect_1.area() > rect_2.area():
             return (rect_1)
         else:
             return (rect_2)
