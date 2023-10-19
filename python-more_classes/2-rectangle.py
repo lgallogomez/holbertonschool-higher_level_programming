22e8e125e85fd9ffdb7341531da45ce2ab61db2f
lgallogomez <l.gallogomez@gmail.com>
2023-10-19 11:03:01 -0500

    created and tested 2nd file

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value == 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,3 +33,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__height + self.__width
