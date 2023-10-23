7f30ec0c5bb0f51bee6d2a32c6ab408c1c4f6b95
lgallogomez <l.gallogomez@gmail.com>
2023-10-23 16:42:28 -0500

    updated 1st file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usd/bin/python3
+
+"""function that returns a lsit of attributes and meths of a class"""
+
 def lookup(obj):
     """function that returns a lsit of attributes and meths of a class"""
-    return dir(obj)
+    return list(dir(obj))
