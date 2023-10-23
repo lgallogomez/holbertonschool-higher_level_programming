aed25401d776fe546b285d7794df6416ccf365d0
lgallogomez <l.gallogomez@gmail.com>
2023-10-23 17:52:05 -0500

    created 2nd file

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,6 +6,9 @@ function returns true if object is same an instance of class, or false
 
 
 def is_same_class(obj, a_class):
+    """
+    function returns true if object is same an instance of class, or false
+    """
     if isinstance(obj, a_class):
         return True
     return False
