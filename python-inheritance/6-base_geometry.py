280ae13c5d9d34cfca0775bb89f88d8ec8f743a6
lgallogomez <l.gallogomez@gmail.com>
2023-10-23 20:27:23 -0500

    created files

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -12,10 +12,3 @@ class BaseGeometry():
 
     def area(self):
         raise Exception("area() is not implemented")
-
-bg = BaseGeometry()
-
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
