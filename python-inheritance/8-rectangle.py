9b5fd5e5b0da420ea0b772e9bf8de5c2fb551a3b
lgallogomez <l.gallogomez@gmail.com>
2023-10-23 21:39:01 -0500

    created files

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,13 +6,12 @@ class Rectangle inherits from BaseGeometry
 
 
 class Rectangle(__import__('7-base_geometry').BaseGeometry):
-'''
-class Rectangle inherits from BaseGeometry
-'''
+    '''
+    class Rectangle inherits from BaseGeometry
+    '''
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-    
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
