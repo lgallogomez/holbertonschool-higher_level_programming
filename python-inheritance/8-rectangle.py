926b4ea2146a7ddf90ee8ff37a86ce0164409f96
lgallogomez <l.gallogomez@gmail.com>
2023-10-23 21:35:48 -0500

    created files

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 '''
 class Rectangle inherits from BaseGeometry
 '''
 
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
 '''
