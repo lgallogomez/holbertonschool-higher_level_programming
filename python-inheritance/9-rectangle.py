a2b7222af1c6e76a7a98d14e47d305f8f845cb62
lgallogomez <l.gallogomez@gmail.com>
2023-10-24 16:43:33 -0500

    updated 9th file and created main

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -21,3 +21,5 @@ class Rectangle(__import__('7-base_geometry').BaseGeometry):
     def __str__(self):
         return("[{}] {}/{}".format(__class__.__name__, self.__width, self.__height))
     
+    def __print__(self):
+        return("[{}] {}/{}".format(__class__.__name__, self.__width, self.__height))
