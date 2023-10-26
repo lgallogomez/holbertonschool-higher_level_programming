d7bf654ef861454282f5126318a08964898b8d1f
lgallogomez <l.gallogomez@gmail.com>
2023-10-26 17:49:11 -0500

    UPDATED 0- file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     Function adds 2 integers. b setted as 98 if nothing is provided
     """
     if a is None:
-        raise NameError("name 'a' is not defined")
+        raise TypeError("a must be an integer")
     if b is None:
-        raise NameError("name 'b' is not defined")
+        raise TypeError("b must be an integer")
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
