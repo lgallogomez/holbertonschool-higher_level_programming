006a4b690c153ff7ab4245e262f895b8323031a0
lgallogomez <l.gallogomez@gmail.com>
2023-10-26 17:36:40 -0500

    UPDATED 0- file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     Function adds 2 integers. b setted as 98 if nothing is provided
     """
 
-    if type(a) is not int and type(a) is not float:
+    if a is None or type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if b is None or type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     c = int(a) + int(b)
     return c
