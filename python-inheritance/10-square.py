b580ef6b479a0dafc343afce5cc95afcba3a8122
lgallogomez <l.gallogomez@gmail.com>
2023-10-26 10:42:11 -0500

    updated 10th file and created main

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,10 +12,11 @@ class Square(__import__('9-rectangle').Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.size = size
+        self.__size = size
 
     def area(self):
         return(self.size ** 2)
 
     def __str__(self):
         return(f"[{__class__.__name__}] {self.size}/{self.size}")
+    
