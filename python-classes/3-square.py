6aece6f7b7b77408f99118da20d1a8ccbb218081
lgallogomez <l.gallogomez@gmail.com>
2023-10-26 11:33:45 -0500

    updated 3th file

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ File where I'm creating a class
 
 
 class Square:
-    """ 
+    """
     class square creates a private attribute
     """
     def __init__(self, size=0):
@@ -15,6 +15,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    
     def area(self):
         a = self.__size * self.__size
         return (a)
