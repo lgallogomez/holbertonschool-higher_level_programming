811f0f93b22d39e1a19a500a95d911cbce391f1e
lgallogomez <l.gallogomez@gmail.com>
2023-10-27 17:47:42 -0500

    updated test file and 0-file

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -5,4 +5,12 @@ print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
-print(add_integer(float('nan')))
+
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
