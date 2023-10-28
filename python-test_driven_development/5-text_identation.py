753f38067f436adb1ee777fcb9d1cd9466b97612
lgallogomez <l.gallogomez@gmail.com>
2023-10-28 18:33:28 -0500

    updated 5th file

---
+++ python-test_driven_development/5-text_identation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+
+
+a = "hola que. mas ome? que ha pasado: todo bien"
+#words = a.split()
+for element in a:
+    if element == '.':
+        print(element)
+        
+    print(element, end="")
+    
