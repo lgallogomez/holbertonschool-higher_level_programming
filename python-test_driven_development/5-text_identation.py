1c3ea2dbb89d8d9ccb249a8e8c7aef3e8f9a1612
lgallogomez <l.gallogomez@gmail.com>
2023-10-30 20:19:59 -0500

    created 5th file

--- python-test_driven_development/5-text_identation.py
+++ python-test_driven_development/5-text_identation.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
+"""
+Function prints a text with 2 new lines
+"""
 
+def text_indentation(text):
+    new_str = ""
+    for element in range(len(text)):
+            if text[element] != '.' and text[element] != '?' and text[element] != ':':
+                new_str += text[element]
+            else:
+                new_str += text[element]
+                while text[element-1] != ' ':
+                    element += 1
+                    new_str += '\n'
+    print (new_str)
 
-
-
-a = "hola que. mas ome? que ha pasado: todo bien"
-#words = a.split()
-for element in a:
-    if element == '.':
-        print(element)
-        
-    print(element, end="")
-    
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
