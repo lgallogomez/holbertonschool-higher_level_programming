23a81f0d76a80fe11bdae6c1a67fde24fa4ea126
lgallogomez <l.gallogomez@gmail.com>
2023-12-15 19:46:43 -0500

    created 7th file

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates db and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     PRIMARY KEY (id),
