c1e875bea14884ada9eb7d8f6be705cdb5513965
lgallogomez <l.gallogomez@gmail.com>
2023-12-15 19:05:18 -0500

    created 6th file

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates database and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL UNIQUE PRIMARY KEY, 
     name VARCHAR(256) NOT NULL
 );
