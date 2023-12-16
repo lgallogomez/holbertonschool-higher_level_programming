d4fb1b79d5aaf7dbd87bde3d55d6c62117fbd2bb
lgallogomez <l.gallogomez@gmail.com>
2023-12-15 19:57:35 -0500

    created 8th file

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities of California that can be found in the database hbtn_0d_usa
 SELECT id
-FROM hbtn_0d_usa.states
-WHERE states.name='California'
+FROM states
+WHERE name='California'
 ORDER BY state_id ASC;
